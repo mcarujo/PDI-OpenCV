05562cba8cf3c2a359361de96af33ed94a3bb152
Marco Carujo <mamcarujo@gmail.com>
2018-03-04 17:29:55 -0300

    seção 2 concluida

---
+++ exercicios/2.2/regions.cpp
@@ -0,0 +1,64 @@
+//Autor: Marco Carujo
+//Data: 02/03/2018
+
+#include <math.h>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int, char **)
+{
+  int p1x, p1y;
+  int p2x, p2y;
+
+  Mat image;
+
+  image = imread("biel.png", CV_LOAD_IMAGE_GRAYSCALE);
+  if (!image.data)
+    cout << "nao abriu a imagem.png" << endl;
+
+  cout << "Porfavor me informa o ponto 1 x e y \n:";
+  cin >> p1x >> p1y;
+
+  cout << "Porfavor me informa o ponto 2 x e y \n :";
+  cin >> p2x >> p2y;
+
+  int px_start, px_finish;
+  int py_start, py_finish;
+  if (p1x < p2x)
+  {
+    px_start = p1x;
+    px_finish = p2x;
+  }
+  else
+  {
+    px_start = p2x;
+    px_finish = p1x;
+  }
+  if (p1y < p2y)
+  {
+    py_start = p1y;
+    py_finish = p2y;
+  }
+  else
+  {
+    py_start = p2y;
+    py_finish = p1y;
+  }
+
+  namedWindow("janela", WINDOW_AUTOSIZE);
+
+  for (int i = px_start; i < px_finish; i++)
+  {
+    for (int j = py_start; j < py_finish; j++)
+    {
+      image.at<uchar>(i, j) = abs(image.at<uchar>(i, j) - 255);
+    }
+  }
+  imshow("janela", image);
+  waitKey();
+
+  return 0;
+}
---
+++ exercicios/2.2/trocaregioes.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <math.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+  Mat image;
+
+  image = imread("biel.png", CV_LOAD_IMAGE_COLOR);
+  if (!image.data)
+    cout << "nao abriu a imagem" << endl;
+
+  int image_width = image.size().width;
+  int image_height = image.size().height;
+
+  Mat superior_esquerdo(image, Rect(0, 0, image_width / 2, image_height / 2));
+  Mat inferior_esquerdo(image, Rect(image_width / 2, 0, image_width / 2, image_height / 2));
+  Mat superior_direito(image, Rect(0, image_height / 2, image_width / 2, image_height / 2));
+  Mat inferior_direito(image, Rect(image_width / 2, image_height / 2, image_width / 2, image_height / 2));
+
+  Mat resultado = Mat::zeros(image.size(), image.type());
+  Mat ponteiro;
+
+  ponteiro = resultado.colRange(image_width / 2, image_width).rowRange(0, image_height / 2);
+  superior_direito.copyTo(ponteiro);
+
+  ponteiro = resultado.colRange(0, image_width / 2).rowRange(image_height / 2, image_height);
+  inferior_esquerdo.copyTo(ponteiro);
+
+  ponteiro = resultado.colRange(image_width / 2, image_width).rowRange(image_height / 2, image_height);
+  superior_esquerdo.copyTo(ponteiro);
+
+  ponteiro = resultado.colRange(0, image_width / 2).rowRange(0, image_height / 2);
+  inferior_direito.clone().copyTo(ponteiro);
+
+  namedWindow("janela", WINDOW_AUTOSIZE);
+  imshow("janela", resultado);
+  waitKey();
+  return 0;
+}
